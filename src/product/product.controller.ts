c62b463770892353743658009658b2b17454d556
Jiwoong <dnd0311@naver.com>
2024-08-06 17:43:33 +0900

    add get/post api

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Get('/all')
+  async getAllProducts() {
+    return await this.productService.getProducts();
+  }
+
+  @Post('/create')
+  async create(@Body() createProductDto: CreateProductDto) {
+    return await this.productService.postProduct(createProductDto);
+  }
+
+  @Get('/:id')
+  async getProductById(@Param('id') id: string) {
+    return await this.productService.getProduct(id);
+  }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Product } from './entities/product.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateProductDto } from './dto/create-product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  ) {}
+
+  async getProducts() {
+    const products = await this.productRepository.find();
+    return { count: products.length, data: products };
+  }
+
+  async postProduct(createProductDto: CreateProductDto) {
+    const newProduct = await this.productRepository.create(createProductDto);
+    await this.productRepository.save(createProductDto);
+    return newProduct;
+  }
+
+  async getProduct(id: string) {
+    const product = await this.productRepository.findOneBy({ id });
+    if (!product) {
+      throw new HttpException('Product not found', HttpStatus.NOT_FOUND);
+    }
+    return product;
+  }
+}
