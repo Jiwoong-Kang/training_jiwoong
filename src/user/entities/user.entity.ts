aaa37a3da65f0695aca148e58a939bd381332f5e
Jiwoong <dnd0311@naver.com>
2024-08-07 10:28:04 +0900

    add password hash and profile image

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,36 @@
-export class User {}
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import { BaseEntity } from '../../common/base.entity';
+import { InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import * as gravatar from 'gravatar';
+@Entity()
+export class User extends BaseEntity {
+  @Column()
+  public name: string;
+
+  @Column()
+  public email: string;
+
+  @Column()
+  public password: string;
+
+  @Column()
+  public profileImg?: string;
+
+  @BeforeInsert()
+  async beforeSaveFunction() {
+    try {
+      const saltValue = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, saltValue);
+
+      this.profileImg = gravatar.url(this.email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+        protocol: 'https',
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+  }
+}
